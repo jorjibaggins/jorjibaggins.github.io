8614e8beb3d8ceb6ee61b4fbd6a480360010aa10
nghui <noreply@github.com>
2025-06-28 18:21:57 -0400

    change some tags

--- src/pages/valuation/+Head.tsx
+++ src/pages/valuation/+Head.tsx
@@ -3,8 +3,8 @@ export { Head }
 function Head() {
   return (
     <>
-      <title>East Street Advisory | Singapore's Top SME Business Broker</title>
-      <meta name="description" content="Professional business valuation services for Singapore SMEs. Get accurate business valuations for M&A transactions, strategic planning, and more." />
+      <title>Free Business Valuation Singapore</title>
+      <meta name="description" content="Get your free professional business valuation for Singapore SMEs. Get accurate business valuations for M&A transactions, strategic planning, and more." />
     </>
   )
 }
