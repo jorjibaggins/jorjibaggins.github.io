a09f14a06abf4bf57eb9f7ea2bb8ad500ebbf97b
nghui <noreply@github.com>
2025-08-28 08:53:18 -0400

    commit

--- src/pages/index/+Head.tsx
+++ src/pages/index/+Head.tsx
@@ -4,7 +4,7 @@ function Head() {
   return (
     <>
       <title>East Street Advisory | Singapore's Top Business Broker</title>
-      <meta name="description" content="Singapore's trusted business broker specializing in M&A and capital advisory, business valuations, and exit strategies. Over S$2B+ in transactions closed." />
+      <meta name="description" content="Singapore's trusted business broker specializing in M&A and capital advisory, business valuations, and exit strategies. Over S$2B+ transactions across 25+ industries." />
     </>
   )
 }
