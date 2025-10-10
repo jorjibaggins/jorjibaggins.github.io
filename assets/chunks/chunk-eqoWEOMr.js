8418d9aa5d27543273c6f6d56a686c8a63f85d90
juxd <juxd@users.noreply.github.com>
2025-10-10 01:39:27 +0000

    deploy: e96420a2efd7d73342df9ccb94f1056d0f420523

--- assets/chunks/chunk-eqoWEOMr.js
+++ assets/chunks/chunk-eqoWEOMr.js
@@ -1,4 +1,4 @@
-import{c as e,j as n,d as o,e as s}from"./chunk-OaWcw_NV.js";/**
+import{c as e,j as n,d as o,e as s}from"./chunk-C_kbdrDa.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
